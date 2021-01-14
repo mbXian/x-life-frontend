e0ec57768c9386dd922d43b2c670e4967cf3e610
Ben <xianminbing@gmail.com>
2021-01-14 16:46:54 +0800

    add: 新建天气页面

---
+++ src/components/KongmoonWeather.vue
@@ -0,0 +1,35 @@
+<template>
+  <div>
+    <h1>{{ msg }}</h1>
+    <h2>Essential Links</h2>
+    <el-button icon="el-icon-search" circle @click="handleClick"></el-button>
+  </div>
+</template>
+
+<script>
+import axios from 'axios';
+
+export default {
+  name: 'KongmoonWeather',
+  data () {
+    return {
+      msg: 'Welcome to Your KongmoonWeather'
+    }
+  },
+  methods: {
+    handleClick() {
+      console.log('点击事件！');
+      axios.get('api/meal/recommend').then(response => {
+          this.$message('请求成功');
+      }, response => {
+          this.$message('请求失败');
+      });
+    }
+  }
+}
+</script>
+
+
+<style scoped>
+
+</style>
